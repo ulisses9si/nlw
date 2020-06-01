d4b2ddcebe432afe64906b4ee96a9d2e59aedb37
Ulisses C <rasracapur@outlook.com>
2020-06-01 15:51:11 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var js = document.querySelector('.js');
+var num = 1;
+if (num == 0) {
+    js.innerHTML = '<p>Testando o Typescript</p>';
+}
+else {
+    js.innerHTML = '<p>Isso é um Typescript</p>';
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let js = document.querySelector('.js');
+let num = 1;
+
+if(num == 0){
+    js.innerHTML = '<p>Testando o Typescript</p>';
+}else{
+    js.innerHTML = '<p>Isso é um Typescript</p>'
+}
+
