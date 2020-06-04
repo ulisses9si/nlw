e1b3c87cdbde5e97962bc4eb125c7c76d5504251
Ulisses C <rasracapur@outlook.com>
2020-06-03 22:01:09 -0300

    adicionado seleção de estado e cidade

--- src/scripts/create-point.js
+++ src/scripts/create-point.js
@@ -3,13 +3,27 @@ function populateUfs() {
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         .then(function (res) { return res.json(); })
         .then(function (states) {
-        ufSelect.innerHTML += "<option value=\"1\">Selecione a Cidade</option>";
+        for (var _i = 0, states_1 = states; _i < states_1.length; _i++) {
+            var state = states_1[_i];
+            ufSelect.innerHTML += "<option value=\"" + state.id + "\">" + state.nome + "</option>";
+        }
         console.log('Criado com suscesso');
     });
 }
+function getCities(event) {
+    var citySelect = document.querySelector('select[name=city]');
+    var url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + event.target.value + "/municipios";
+    console.log(event.target.value);
+    console.log(url);
+    fetch(url).then(function (res) { return res.json(); }).then(function (cities) {
+        for (var _i = 0, cities_1 = cities; _i < cities_1.length; _i++) {
+            var city = cities_1[_i];
+            citySelect.innerHTML += "<option value=\"" + city.id + "\">" + city.nome + "</option>";
+        }
+    });
+    citySelect.disabled = false;
+}
 populateUfs();
-// document
-//     .querySelector('select[name=UF]')
-//     .addEventListener('change',() => { 
-//         console.log('mudei!!!') 
-//     })
+document
+    .querySelector('select[name=UF]')
+    .addEventListener('change', getCities);
--- src/scripts/create-point.ts
+++ src/scripts/create-point.ts
@@ -1,12 +1,34 @@
 function populateUfs(){
     const ufSelect = document.querySelector('select[name=UF]')
+    const stateInput = document.querySelector('input[name=state]')
 
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         .then((res) => res.json())
         .then((states) => {
-            ufSelect.innerHTML += `<option value="1">Selecione a Cidade</option>`
+            for(let state of states){
+               ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
             console.log('Criado com suscesso')
         })
+}
+
+function getCities(event){
+    const citySelect = document.querySelector('select[name=city]')
+    const cityInput = document.querySelector('input[name=city]')
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`
+
+
+    console.log(event.target.value)
+    console.log(url)
+
+    fetch(url).then(res => res.json()).then(cities => {
+        for(let city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+    })
+
+    // citySelect.disabled = false
+
 }
 
 
@@ -14,10 +36,8 @@ populateUfs()
 
 
 
-// document
-//     .querySelector('select[name=UF]')
-//     .addEventListener('change',() => { 
-//         console.log('mudei!!!') 
-//     })
+document
+    .querySelector('select[name=UF]')
+    .addEventListener('change', getCities)
 
 
