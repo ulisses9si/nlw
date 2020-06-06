2bb4dbd12cec953aaffef104c5f1529545662bbd
Ulisses Cardoso <ulisses.scdasilva@gmail.com>
2020-06-06 02:07:11 -0300

    Parte deo frontend finaaaaaaaaaalmente terminada

--- src/scripts/JavaScript/searchResult.js
+++ src/scripts/JavaScript/searchResult.js
@@ -3,9 +3,10 @@ var resultQunt = document.querySelector('#search-result #ponto-cards h3 strong')
 var cards = document.querySelector('#search-result #ponto-cards .cards');
 var num = 3;
 var ramdomNum = Math.floor(Math.random() * (100 - 1) + 1);
-resultQunt.innerHTML = num + " Pontos";
-cards.innerHTML += "\n<div class=\"cards\">\n    <img src=\"https://picsum.photos/id/" + ramdomNum + "/200\" alt=\"Colectoria\">\n    <h2>tristique, eros ac sodales convallis</h2>\n    <h4>eros ac sodales</h4>\n    <p>\n    Aenean feugiat erat lorem, ut accumsan risus dapibus ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n    Vivamus auctor molestie imperdiet. \n    Nullam est est, euismod a ex vitae, semper feugiat neque.\n    Suspendisse posuere tellus nec lectus tincidunt malesuada. \n    Cras tempus mi et ipsum auctor, nec dapibus libero condimentum.\n    </p>\n</div>\n";
+var tam = 300;
+resultQunt.innerHTML = num + 2 + " Pontos";
+cards.innerHTML += "\n<div class=\"card\">\n    <img src=\"https://picsum.photos/id/" + ramdomNum + "/" + tam + "/" + tam + "\" alt=\"Colectoria\">\n    <h2>tristiqulis</h2>\n    <h4>eros ac sodales</h4>\n    <p>\n    Aenean feugiat eraiscing elit.<br> \n    Vivamus auctor molestie imperdiet.<br>\n    Nullam est estr feugiat neque.<br>\n\n    </p>\n</div>\n";
 for (var i = 0; i <= num; i++) {
     ramdomNum = Math.floor(Math.random() * (100 - 1) + 1);
-    cards.innerHTML += "\n        <div class=\"cards\">\n        <img src=\"https://picsum.photos/id/" + ramdomNum + "/200\" alt=\"Colectoria\">\n        <h2>tristique, eros ac sodales convallis</h2>\n        <h4>eros ac sodales</h4>\n        <p>\n        Aenean feugiat erat lorem, ut accumsan risus dapibus ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n        Vivamus auctor molestie imperdiet. \n        Nullam est est, euismod a ex vitae, semper feugiat neque.\n        Suspendisse posuere tellus nec lectus tincidunt malesuada. \n        Cras tempus mi et ipsum auctor, nec dapibus libero condimentum.\n        </p>\n    </div>\n";
+    cards.innerHTML += "\n        <div class=\"card\">\n        <img src=\"https://picsum.photos/id/" + ramdomNum + "/" + tam + "/" + tam + "\" alt=\"Colectoria\">\n        <h2>tristiqu</h2>\n        <h4>eros ac sodales</h4>\n        <p>\n        Aenean feugor sipiscing elit.<br> \n        Vivamus auctor molestie imperdiet. <br>\n        Nullam est estr feugiat neque.<br>\n        </p>\n    </div>\n";
 }
--- src/scripts/TypeScript/searchResult.ts
+++ src/scripts/TypeScript/searchResult.ts
@@ -5,20 +5,20 @@ let cards = document.querySelector('#search-result #ponto-cards .cards');
 
 let num = 3;
 let ramdomNum = Math.floor(Math.random() * (100 - 1) + 1)
+let tam = 300
 
-resultQunt.innerHTML = `${num} Pontos`
+resultQunt.innerHTML = `${num + 2} Pontos`
 
 cards.innerHTML += `
-<div class="cards">
-    <img src="https://picsum.photos/id/${ramdomNum}/200" alt="Colectoria">
-    <h2>tristique, eros ac sodales convallis</h2>
+<div class="card">
+    <img src="https://picsum.photos/id/${ramdomNum}/${tam}/${tam}" alt="Colectoria">
+    <h2>tristiqulis</h2>
     <h4>eros ac sodales</h4>
     <p>
-    Aenean feugiat erat lorem, ut accumsan risus dapibus ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> 
+    Aenean feugiat eraiscing elit.<br> 
     Vivamus auctor molestie imperdiet.<br>
-    Nullam est est, euismod a ex vitae, semper feugiat neque.<br>
-    Suspendisse posuere tellus nec lectus tincidunt malesuada. <br>
-    Cras tempus mi et ipsum auctor, nec dapibus libero condimentum.<br>
+    Nullam est estr feugiat neque.<br>
+
     </p>
 </div>
 `
@@ -26,16 +26,14 @@ cards.innerHTML += `
 for(let i = 0; i <= num; i++){
     ramdomNum = Math.floor(Math.random() * (100 - 1) + 1)
     cards.innerHTML += `
-        <div class="cards">
-        <img src="https://picsum.photos/id/${ramdomNum}/200" alt="Colectoria">
-        <h2>tristique, eros ac sodales convallis</h2>
+        <div class="card">
+        <img src="https://picsum.photos/id/${ramdomNum}/${tam}/${tam}" alt="Colectoria">
+        <h2>tristiqu</h2>
         <h4>eros ac sodales</h4>
         <p>
-        Aenean feugiat erat lorem, ut accumsan risus dapibus ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> 
+        Aenean feugor sipiscing elit.<br> 
         Vivamus auctor molestie imperdiet. <br>
-        Nullam est est, euismod a ex vitae, semper feugiat neque.<br>
-        Suspendisse posuere tellus nec lectus tincidunt malesuada.<br> 
-        Cras tempus mi et ipsum auctor, nec dapibus libero condimentum.<br>
+        Nullam est estr feugiat neque.<br>
         </p>
     </div>
 ` 
